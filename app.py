de3b7945b46dccf4f3ffc96424be15b5cf5af8db
Andrea Morgan <morgan.andrea.8984@gmail.com>
2019-10-08 20:21:36 -0500

    adding app.py changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify, render_template
 
@@ -17,7 +17,7 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-engine = create_engine("sqlite:///Project2/data/BOTULISM.db")
+engine = create_engine("sqlite:///data/BOTULISM.db")
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -84,6 +84,163 @@ def statenames():
 
     return jsonify(state_names)
 
+@app.route("/statecharts/<state>")
+#Route returns data for two state charts:  bot type vs time, tox type vs time
+def stateInfo(state):
+    #Returns data for one specific state
+
+    session = Session(engine)
+
+    state_data = session.query(bot_data.state_name, 
+                        bot_data.record_year, 
+                        bot_data.BotType,
+                        bot_data.ToxinType,
+                        bot_data.record_count).filter(bot_data.state_name == state).all()
+    
+    session.close()
+
+    years = []
+
+    foodborne_bot = []
+    infant_bot = []
+    wound_bot = []
+    other_bot = []
+
+    unknown_tox = []
+    E_tox =[]
+    B_tox =[]
+    A_tox =[]
+    F_tox =[]
+    AB_tox =[]
+    Ba_tox =[]
+    Bf_tox =[]
+    EF_tox =[]
+    ABE_tox =[]
+    Ab_tox =[]
+    BF_tox =[]
+
+    for i in state_data:
+        years.append(i[1])
+
+    for i in state_data:
+        if i[2] == "Foodborne":
+            foodborne_bot.append(i[4])
+        else:
+            foodborne_bot.append('0')
+            
+    for i in state_data:
+        if i[2] == "Infant":
+            infant_bot.append(i[4])
+        else:
+            infant_bot.append('0')
+            
+    for i in state_data:
+        if i[2] == "Wound":
+            wound_bot.append(i[4])
+        else:
+            wound_bot.append('0')
+            
+    for i in state_data:
+        if i[2] == "Other":
+            other_bot.append(i[4])
+        else:
+            other_bot.append('0')
+    
+    for i in state_data:
+        if i[3] == "Unknown":
+            unknown_tox.append(i[4])
+        else:
+            unknown_tox.append('0')
+        
+    for i in state_data:
+        if i[3] == "E":
+            E_tox.append(i[4])
+        else:
+            E_tox.append('0')
+            
+    for i in state_data:
+        if i[3] == "B":
+            B_tox.append(i[4])
+        else:
+            B_tox.append('0')
+            
+    for i in state_data:
+        if i[3] == "A":
+            A_tox.append(i[4])
+        else:
+            A_tox.append('0')
+            
+    for i in state_data:
+        if i[3] == "F":
+            F_tox.append(i[4])
+        else:
+            F_tox.append('0')
+            
+    for i in state_data:
+        if i[3] == "AB":
+            AB_tox.append(i[4])
+        else:
+            AB_tox.append('0')
+            
+    for i in state_data:
+        if i[3] == "Ba":
+            Ba_tox.append(i[4])
+        else:
+            Ba_tox.append('0')
+            
+    for i in state_data:
+        if i[3] == "Bf":
+            Bf_tox.append(i[4])
+        else:
+            Bf_tox.append('0')
+            
+    for i in state_data:
+        if i[3] == "EF":
+            EF_tox.append(i[4])
+        else:
+            EF_tox.append('0')
+            
+    for i in state_data:
+        if i[3] == "ABE":
+            ABE_tox.append(i[4])
+        else:
+            ABE_tox.append('0')
+            
+    for i in state_data:
+        if i[3] == "Ab":
+            Ab_tox.append(i[4])
+        else:
+            Ab_tox.append('0')
+            
+    for i in state_data:
+        if i[3] == "BF":
+            BF_tox.append(i[4])
+        else:
+            BF_tox.append('0')
+
+    state_dict = {
+        "years": years,
+        "foodborne": foodborne_bot,
+        "infant": infant_bot,
+        "wound": wound_bot,
+        "other": other_bot,
+        "unknownTox": unknown_tox,
+        "ETox": E_tox,
+        "BTox": B_tox,
+        "ATox": A_tox,
+        "FTox": F_tox,
+        "ABTox": AB_tox,
+        "BaTox": Ba_tox,
+        "BfTox": Bf_tox,
+        "EFTox": EF_tox,
+        "ABETox": ABE_tox,
+        "AbTox": Ab_tox,
+        "BFTox": BF_tox
+    }
+    
+    return jsonify(state_dict)
+        
+
 @app.route("/bottypes")
 #Route returns list of unique Botulism types
 def botTypes():
@@ -142,6 +299,135 @@ def record_years():
 
 
     return jsonify(years)
+
+@app.route("/circles")
+#Returns json for circlechart
+
+def circleData():
+    session = Session(engine)
+
+    all_states = session.query(bot_data.state_name).distinct().all()
+
+    state_names = []
+    
+    for i in all_states:
+        for state in i:
+            state_names.append(state)
+
+
+    food_children = []
+    for state in state_names:
+    
+    
+        foodborne_data = session.query(bot_data.state_name,
+                                bot_data.BotType,
+                            bot_data.record_year,
+                            func.sum(bot_data.record_count)).\
+                            filter(bot_data.BotType == "Foodborne").\
+                            filter(bot_data.state_name == state).\
+                            group_by(bot_data.record_year).\
+                            order_by(bot_data.state_name).all()
+        
+        children = []
+        
+        for i in foodborne_data:
+
+            children.append({"name": i[2], "value": i[3]})
+            
+
+        one_state = {"name": state, "children": children}  
+        food_children.append(one_state)
+
+
+    infant_children = []
+    for state in state_names:
+    
+    
+        infant_data = session.query(bot_data.state_name,
+                                bot_data.BotType,
+                            bot_data.record_year,
+                            func.sum(bot_data.record_count)).\
+                            filter(bot_data.BotType == "Infant").\
+                            filter(bot_data.state_name == state).\
+                            group_by(bot_data.record_year).\
+                            order_by(bot_data.state_name).all()
+        
+        children = []
+        
+        for i in infant_data:
+
+            children.append({"name": i[2], "value": i[3]})
+            
+
+        one_state = {"name": state, "children": children}  
+        infant_children.append(one_state)
+        
+    wound_children = []
+    for state in state_names:
+    
+    
+        wound_data = session.query(bot_data.state_name,
+                                bot_data.BotType,
+                            bot_data.record_year,
+                            func.sum(bot_data.record_count)).\
+                            filter(bot_data.BotType == "Wound").\
+                            filter(bot_data.state_name == state).\
+                            group_by(bot_data.record_year).\
+                            order_by(bot_data.state_name).all()
+        
+        children = []
+        
+        for i in wound_data:
+
+            children.append({"name": i[2], "value": i[3]})
+            
+
+        one_state = {"name": state, "children": children}  
+        wound_children.append(one_state)
+
+    other_children = []
+    for state in state_names:
+    
+    
+        other_data = session.query(bot_data.state_name,
+                                bot_data.BotType,
+                            bot_data.record_year,
+                            func.sum(bot_data.record_count)).\
+                            filter(bot_data.BotType == "Other").\
+                            filter(bot_data.state_name == state).\
+                            group_by(bot_data.record_year).\
+                            order_by(bot_data.state_name).all()
+        
+        children = []
+        
+        for i in other_data:
+
+            children.append({"name": i[2], "value": i[3]})
+            
+        one_state = {"name": state, "children": children}  
+        other_children.append(one_state)
+
+    session.close()
+
+    json= {"name": "botulism",
+       "children":[
+           {
+           "name": "Foodborne",
+            "children": food_children
+           },
+           {
+            "name": "Infant",
+            "children": infant_children
+           },
+           {"name": "Wound",
+            "children": wound_children
+           },
+           {"name": "Other",
+           "children": other_children}
+       ]}
+    
+    return jsonify(json)
+
 
 
 if __name__ == "__main__":
